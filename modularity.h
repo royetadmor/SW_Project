026e8d7fbc3b07e5dcfcba468f0b5fe2d601640b
HadarTal <hadar.tal.isr@gmail.com>
2020-08-18 15:23:08 +0300

    add the modularity.h header

---
+++ modularity.h
@@ -0,0 +1,16 @@
+/*
+ * modularity.h
+ *
+ *  Created on: 18 Aug 2020
+ *      Author: hadar
+ */
+
+#ifndef MODULARITY_H_
+#define MODULARITY_H_
+
+
+int** getModularityMatrix(int **matrix);
+
+
+
+#endif /* MODULARITY_H_ */
