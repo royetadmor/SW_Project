c4126eb4a2568a2707e412d4dc2a56c93057fef3
Roye <tadmoroye@gmail.com>
2020-08-24 15:57:46 +0300

    Kosheleyimashcha

---
+++ input.h
@@ -0,0 +1,11 @@
+/*
+* Created by Roye on 19/08/2020.
+*/
+
+#ifndef SW_PROJECT_INPUT_H
+#define SW_PROJECT_INPUT_H
+
+
+int getInputMatrix(char* binaryInput, int *lengthPointer, int* degreesSum, int** degreesArray, int***matrixPointer);
+
+#endif /*SW_PROJECT_INPUT_H*/
---
+++ modularity.h
@@ -0,0 +1,16 @@
+/*
+ * modularity.h
+ *
+ *  Created on: 18 Aug 2020
+ *      Author: hadar
+ */
+
+#ifndef MODULARITY_H_
+#define MODULARITY_H_
+
+
+int getModularityMatrix(double ***multMatrix, int length, int degreesSum, int* degreesArray, int**matrix);
+
+
+
+#endif /* MODULARITY_H_ */
