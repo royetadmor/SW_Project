279781e48031cd594f0a39f05260afbaefc840ce
HadarTal <hadar.tal.isr@gmail.com>
2020-09-03 16:14:23 +0300

    modularity.h

--- modularity.h
+++ modularity.h
@@ -4,13 +4,15 @@
  *  Created on: 18 Aug 2020
  *      Author: hadar
  */
+#include "List.h"
+
 
 #ifndef MODULARITY_H_
 #define MODULARITY_H_
 
 
-int getModularityMatrix(double ***multMatrix, int length, int degreesSum, int* degreesArray, int**matrix);
-
+int getModularityMatrix(double ***modMatrix, int length, int degreesSum, int* degreesArray, int**matrix);
+int getBTagMatrix(double ***BTagMatrix, double **modMatrix, list *indices);
 
 
 #endif /* MODULARITY_H_ */
